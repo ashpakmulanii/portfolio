0e73872b17a7168ac662d35426a4b1fcfd37ad3e
ashpak <ashpakmulani1101@gmail.com>
2021-11-25 19:06:13 +0530

    Updates

---
+++ precache-manifest.fd28980776ce06472645b7c2f88bc809.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eea7811e46333933c16e0a734df70a3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fb3ca6c09a807fc7d86d",
+    "url": "/portfolio/static/css/main.56bbd17d.chunk.css"
+  },
+  {
+    "revision": "dfa427a2e168926727a8",
+    "url": "/portfolio/static/js/2.2a2e5ad2.chunk.js"
+  },
+  {
+    "revision": "0e6cb1fbd5179720173d2f6b0067c84b",
+    "url": "/portfolio/static/js/2.2a2e5ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb3ca6c09a807fc7d86d",
+    "url": "/portfolio/static/js/main.cb9d1a59.chunk.js"
+  },
+  {
+    "revision": "1a5ad2fa4121c24d322d",
+    "url": "/portfolio/static/js/runtime-main.3fc07cb0.js"
+  }
+]);
