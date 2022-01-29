52f7f65b78191caf3662acbec65124954f96774f
ashpakmulanii <ashpakmulani1101@gmail.com>
2022-01-29 15:24:39 +0530

    Updates

---
+++ precache-manifest.d05fe42783420e0b1c6df96e4909634f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4ebd9abd2a9907b4898f5716d36724",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cac6d8234dbde613503e",
+    "url": "/portfolio/static/css/main.a0801cec.chunk.css"
+  },
+  {
+    "revision": "5a6917f25634832be186",
+    "url": "/portfolio/static/js/2.b9362b7c.chunk.js"
+  },
+  {
+    "revision": "ed961b1d682fddd39f72e5d7dd46c4a2",
+    "url": "/portfolio/static/js/2.b9362b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac6d8234dbde613503e",
+    "url": "/portfolio/static/js/main.6449b8ef.chunk.js"
+  },
+  {
+    "revision": "1a5ad2fa4121c24d322d",
+    "url": "/portfolio/static/js/runtime-main.3fc07cb0.js"
+  },
+  {
+    "revision": "c5b3b2cfc5fe8c7261a1a27943a69bb0",
+    "url": "/portfolio/static/media/11.c5b3b2cf.jpg"
+  },
+  {
+    "revision": "0adf78bba00d4b885ea7e3898ffa3af5",
+    "url": "/portfolio/static/media/publication.0adf78bb.JPG"
+  },
+  {
+    "revision": "1a26183054985e4776c31e9ec4d21556",
+    "url": "/portfolio/static/media/ranker.1a261830.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5c1bc2c5fb40f07fa02df537e8908825.js"
+  "/portfolio/precache-manifest.d05fe42783420e0b1c6df96e4909634f.js"
 );
 
 self.addEventListener('message', (event) => {
