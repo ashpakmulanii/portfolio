51000a248a3330e0fcd84154a32d18efb8a02d23
ashpakmulanii <ashpakmulani1101@gmail.com>
2022-02-20 15:02:31 +0530

    navbar fixes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,89 +6,91 @@ const Nav = () => {
   const [state, setState] = React.useState(false);
   return (
     <nav className="navbar">
-      <div className="navbar__container">
-        <ul className="navbar__left">
-          <div className="navbar__title">
-            <FaCode style={{ fontSize: "27px", color: "silver", paddingRight: "5px" }} />
-            <span className="Span">Ashpak Mulani</span>
-          </div>
-        </ul>
-        <ul className="ln">
-          <Link className="nav-btn-outline" style={{ fontWeight: "450", color: "white" }} to="/portfolio"
-          >
-            Home
-          </Link>
-          <Link className="nav-btn-outline" style={{ color: "#F9F7FC" }} to="/publications">
-            Archievements
-          </Link>
-          <Link className="nav-btn-outline" to="/projects">
-            Projects
-          </Link>
-          <Link className="nav-btn-outline" to="/services">
-            Skills
-          </Link>
-        </ul>
-
-        <ul type="none" className="navbar__icons">
-          <li>
-            <a href="https://www.linkedin.com/in/ashpak-mulani/">
-              <FaLinkedinIn className="nav_ico" />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/ashpakmulanii">
-              <FaGithub className="nav_ico" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/ashpakmulanii/">
-              <FaInstagram className="nav_ico" />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.facebook.com/ashpak.mulani.3994/">
-              <FaFacebookF class="nav_ico" />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/AshpakMulanii">
-              <FaTwitter className="nav_ico" />
-            </a>
-          </li>
-          <li>
-            <a className="nav__btn" href="https://staticg.sportskeeda.com/editor/2022/02/97966-16441723251640-1920.jpg" download>Download Resume</a>
-          </li>
-        </ul>
-
-        {state ? (
-          <ul className="navbar__right">
-            <li>
-              <Link to="/portfolio" onClick={() => setState(!state)}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/publications" onClick={() => setState(!state)}>Honors and Archievements</Link>
-            </li>
-            <li>
-              <Link to="/services" onClick={() => setState(!state)}>Services</Link>
-            </li>
-            <li>
-              <Link to="/about" onClick={() => setState(!state)}>About</Link>
-            </li>
-            <li>
-              <Link to="/services" onClick={() => setState(!state)}>Tech Skills</Link>
-            </li>
-
-            <li>
-              <Link to="/projects" onClick={() => setState(!state)}>Projects</Link>
-            </li>
-
+      <div className="container">
+        <div className="navbar__container">
+          <ul className="navbar__left">
+            <div className="navbar__title">
+              <FaCode style={{ fontSize: "27px", color: "silver", paddingRight: "5px" }} />
+              <span className="Span">ApkWebX</span>
+            </div>
           </ul>
-        ) : (
-          ""
-        )}
+          <ul className="ln">
+            <Link className="nav-btn-outline" style={{ fontWeight: "450", color: "white" }} to="/portfolio"
+            >
+              Home
+            </Link>
+            <Link className="nav-btn-outline" style={{ color: "#F9F7FC" }} to="/publications">
+              Archievements
+            </Link>
+            <Link className="nav-btn-outline" to="/projects">
+              Projects
+            </Link>
+            <Link className="nav-btn-outline" to="/services">
+              Skills
+            </Link>
+          </ul>
 
+          <ul type="none" className="navbar__icons">
+            <li>
+              <a href="https://www.linkedin.com/in/ashpak-mulani/">
+                <FaLinkedinIn className="nav_ico" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/ashpakmulanii">
+                <FaGithub className="nav_ico" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/ashpakmulanii/">
+                <FaInstagram className="nav_ico" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/ashpak.mulani.3994/">
+                <FaFacebookF class="nav_ico" />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/AshpakMulanii">
+                <FaTwitter className="nav_ico" />
+              </a>
+            </li>
+            <li>
+              <a className="nav__btn" href="https://staticg.sportskeeda.com/editor/2022/02/97966-16441723251640-1920.jpg" download>Download Resume</a>
+            </li>
+          </ul>
+
+          {state ? (
+            <ul className="navbar__right">
+              <li>
+                <Link to="/portfolio" onClick={() => setState(!state)}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/publications" onClick={() => setState(!state)}>Honors and Archievements</Link>
+              </li>
+              <li>
+                <Link to="/services" onClick={() => setState(!state)}>Services</Link>
+              </li>
+              <li>
+                <Link to="/about" onClick={() => setState(!state)}>About</Link>
+              </li>
+              <li>
+                <Link to="/services" onClick={() => setState(!state)}>Tech Skills</Link>
+              </li>
+
+              <li>
+                <Link to="/projects" onClick={() => setState(!state)}>Projects</Link>
+              </li>
+
+            </ul>
+          ) : (
+            ""
+          )}
+
+        </div>
       </div>
       <div className="toggle__box" onClick={() => setState(!state)}>
         {!state ? (<FaAlignJustify className="toggle__btn" />) : (
