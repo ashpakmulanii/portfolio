edd2b32b9c4ff682f69a8dc93a8555c2b3a65233
ashpakmulanii <ashpakmulani1101@gmail.com>
2022-02-24 15:02:18 +0530

    Updates

--- precache-manifest.04ae4cc3d19c630ef0780a1922b7871f.js
+++ precache-manifest.04ae4cc3d19c630ef0780a1922b7871f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0fef60e7769b2af1738186f43e139f65",
+    "revision": "cdfe6845610fde55b0f605ef33cb10f6",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "b0a4e4351d1a905086e0",
-    "url": "/portfolio/static/css/main.95b5d741.chunk.css"
+    "revision": "8378aedd2cb2cbc00a93",
+    "url": "/portfolio/static/css/main.629c7180.chunk.css"
   },
   {
-    "revision": "140f49c9ab94b4710305",
-    "url": "/portfolio/static/js/2.c6e4f321.chunk.js"
+    "revision": "109b267c50848141e9f3",
+    "url": "/portfolio/static/js/2.8d75e16c.chunk.js"
   },
   {
     "revision": "ed961b1d682fddd39f72e5d7dd46c4a2",
-    "url": "/portfolio/static/js/2.c6e4f321.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.8d75e16c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b0a4e4351d1a905086e0",
-    "url": "/portfolio/static/js/main.e4961d7f.chunk.js"
+    "revision": "8378aedd2cb2cbc00a93",
+    "url": "/portfolio/static/js/main.2b895cc1.chunk.js"
   },
   {
     "revision": "d410790a09206739e0bc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e32010edb9ef407d2cf7faabd9dba255.js"
+  "/portfolio/precache-manifest.04ae4cc3d19c630ef0780a1922b7871f.js"
 );
 
 self.addEventListener('message', (event) => {
