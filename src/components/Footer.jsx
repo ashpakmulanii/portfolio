6552584becccc7965a28b27e3e0d81d05fd08f1e
ashpakmulanii <ashpakmulani1101@gmail.com>
2022-02-25 11:30:30 +0530

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ export const Footer = () => {
         <footer className='footer'>
             <div className="footer__container">
                 <ul className="footer__left">
-                    <div className="navbar__title">
+                    <div className="footer__title">
                         <span className="Span">Apk </span> <p className="sublogo">JSDev</p>
                     </div>
                 </ul>
                 <ul className="footer__right">
-                    <a>&copy;Copyright all rights reserved by ashpakmulani 2021-2025</a>
+                    <a>&copy;Copyright 2021-2025</a>
                 </ul>
             </div>
         </footer >
